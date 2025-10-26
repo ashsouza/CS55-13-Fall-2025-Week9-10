04e49d55d148d72179c126a17d0943eb5959bf3d
ashsouza <asouza@bearcubs.santarosa.edu>
2025-10-25 19:55:43 -0700

    restaurant reviews

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -34,11 +34,50 @@ const updateWithRating = async (
   newRatingDocument,
   review
 ) => {
-  return;
+  const recipe = await transaction.get(docRef);
+  const data = recipe.data();
+  const newNumRatings = data?.numRatings ? data.numRatings + 1 : 1;
+  const newSumRating = (data?.sumRating || 0) + Number(review.rating);
+  const newAverage = newSumRating / newNumRatings;
+
+  transaction.update(docRef, {
+    numRatings: newNumRatings,
+    sumRating: newSumRating,
+    avgRating: newAverage,
+  });
+
+  transaction.set(newRatingDocument, {
+    ...review,
+    timestamp: Timestamp.fromDate(new Date()),
+  });
 };
 
 export async function addReviewToRecipe(db, recipeId, review) {
-  return;
+  if (!recipeId) {
+    throw new Error("No recipe ID has been provided.");
+  }
+
+  if (!review) {
+    throw new Error("A valid review has not been provided.");
+  }
+
+  try {
+    const docRef = doc(collection(db, "recipes"), recipeId);
+    const newRatingDocument = doc(
+      collection(db, `recipes/${recipeId}/ratings`)
+    );
+
+    // corrected line
+    await runTransaction(db, transaction =>
+      updateWithRating(transaction, docRef, newRatingDocument, review)
+    );
+  } catch (error) {
+    console.error(
+      "There was an error adding the rating to the recipe",
+      error
+    );
+    throw error;
+  }
 }
 
 function applyQueryFilters(q, { category, difficulty, sort }) {
